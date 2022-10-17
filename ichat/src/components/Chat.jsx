9f7d771147db9ff805f0eb4b6fa4226080aff0f9
ahmedjanwar <ajemal840@gamil.com>
2022-10-17 21:28:40 +0300

    project done some bugs and adding comnts left

--- ichat/src/components/Chat.jsx
+++ ichat/src/components/Chat.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from "react";
 import Cam from "../images/Cam.png"
 import Add from "../images/Add.png"
 import More from "../images/More.jpg"
-import Messages from './Messages'
-import Input from './Input'
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 function Chat() {
+  const { data } = useContext(ChatContext);
   return (
     <div className='chat'>
       <div className="chatInfo">
-        <span>Hamood</span>
+      <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           {/** 
           <img src={Cam} alt="" />
--- ichat/src/components/Chats.jsx
+++ ichat/src/components/Chats.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
 
-function Chats() {
+const Chats = () => {
+  const [chats, setChats] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  console.log(chats)
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
   return (
-    <div className='chats'>
-      {/**---------------- */}
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/photos/young-male-skateboarder-at-skate-park-picture-id1363625946?b=1&k=20&m=1363625946&s=170667a&w=0&h=nAc971_mOPSG0RMDvThx3J2f__7k_fEOI660l2BVMYg=" alt="" />
-        <div className="userChatInfo">
-          <span>Ahmed</span>
-          <p>hello</p>
+    <div className="chats">
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+        <div
+          className="userChat"
+          key={chat[0]}
+         onClick={() => handleSelect(chat[1].userInfo)}
+        >
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      {/**---------------- */}
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/photos/young-male-skateboarder-at-skate-park-picture-id1363625946?b=1&k=20&m=1363625946&s=170667a&w=0&h=nAc971_mOPSG0RMDvThx3J2f__7k_fEOI660l2BVMYg=" alt="" />
-        <div className="userChatInfo">
-          <span>jemal</span>
-          <p>hello</p>
-        </div>
-      </div>
-      {/**---------------- */}
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/photos/young-male-skateboarder-at-skate-park-picture-id1363625946?b=1&k=20&m=1363625946&s=170667a&w=0&h=nAc971_mOPSG0RMDvThx3J2f__7k_fEOI660l2BVMYg=" alt="" />
-        <div className="userChatInfo">
-          <span>Anwar</span>
-          <p>hello</p>
-        </div>
-      </div>
-      {/**---------------- */}
-      <div className="userChat">
-        <img src="https://media.istockphoto.com/photos/young-male-skateboarder-at-skate-park-picture-id1363625946?b=1&k=20&m=1363625946&s=170667a&w=0&h=nAc971_mOPSG0RMDvThx3J2f__7k_fEOI660l2BVMYg=" alt="" />
-        <div className="userChatInfo">
-          <span>Adem</span>
-          <p>hello</p>
-        </div>
-      </div>
-      {/**---------------- */}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
